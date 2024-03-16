949a1dca41db68085b6154540b70efa4a8a1431b
Samuel <wongsamuel12321@gmail.com>
2024-03-16 13:39:48 +0800

    submit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests
 
-name = input('Your name: ')
+name = input("Your name: ")
 
-response = requests.get(f'https://api.agify.io?name={name}')
-
-data = response.json()
-age = data['age']
-
-response = requests.get(f'https://api.genderize.io?name={name}')
+response = requests.get(f"https://api.agify.io?name={name}")
 
 data = response.json()
-gender = data['gender']
+age = data["age"]
 
-response = requests.get(f'https://api.nationalize.io?name={name}')
+response = requests.get(f"https://api.genderize.io?name={name}")
+
+data = response.json()
+gender = data["gender"]
+
+response = requests.get(f"https://api.nationalize.io?name={name}")
 
 data = response.json()
 
@@ -24,4 +24,6 @@ for country in data["country"]:
         max_probability = country["probability"]
         country_with_max_probability = country["country_id"]
 
-print(f'You are probably a {age} years old {gender} from {country_with_max_probability}.')
+print(
+    f"You are probably a {age} years old {gender} from {country_with_max_probability}."
+)
